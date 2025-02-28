c6d32e029df3a7e49c952d408ea80454bd05ec6f
Corfliss <fauzanhanandito@gmail.com>
2025-02-28 16:56:58 +0700

    gdlint the whole thing

--- scenes/GameSettings.gd
+++ scenes/GameSettings.gd
@@ -1,16 +1,5 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass  # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 # Assitance with ChatGPT, refer to: https://chatgpt.com/share/67b80003-f7fc-8002-8101-713356e7b30f
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed and event.keycode == KEY_CTRL:
